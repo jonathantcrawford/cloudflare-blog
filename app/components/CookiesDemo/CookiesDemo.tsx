97c3774b1879fce5ab20e8856d724b5abc4aac19
jonathantcrawford <jon_t_crawford@protonmail.com>
2022-01-27 08:11:46 -0600

    some refactoring

---
+++ app/components/CookiesDemo/CookiesDemo.tsx
@@ -0,0 +1,86 @@
+
+
+import { Form, useOutletContext, useTransition } from "remix";
+
+import { AnimatePresence, motion } from "framer-motion";
+
+import { CodeSnippet  } from "~/components/CodeSnippet/CodeSnippet";
+
+
+export function CookiesDemo() {
+ const { metaData, contract } = useOutletContext<any>();
+
+ const transition = useTransition();
+
+ const {state} = transition;
+
+ const isLoading = state !== "idle" ;
+
+
+
+  return (
+    <div className="flex flex-wrap-reverse flex-direction-row align-items-center justify-content-center">
+    <div style={{maxWidth: "500px", width: "100%"}} className="flex flex-direction-column align-items-center">
+        <Form className="w-100p" method="post" action="/blog/experiments-with-remix-and-cloudflare-workers" replace>
+        <input type="hidden" name="contract" value="0xd5dfb159788856f9fd5f897509d5a68b7b571ea8" />
+        <input type="hidden" name="tokenId" value="0x0e89341c0000000000000000000000000000000000000000000000000000000000000004" />
+        
+        <button
+            type="submit"
+            className="button w-100p m-b-1rem"
+            disabled={contract == "0xd5dfb159788856f9fd5f897509d5a68b7b571ea8"}
+        >
+            Tacoshi Nakamoto
+        </button>
+        </Form>
+        <Form className="w-100p" method="post" action="/blog/experiments-with-remix-and-cloudflare-workers" replace>
+        <input type="hidden" name="contract" value="0xD5Dfb159788856f9fd5F897509d5a68b7b571Ea8" />
+        <input type="hidden" name="tokenId" value="0x0e89341c0000000000000000000000000000000000000000000000000000000000000009" />
+        <button
+            type="submit"
+            className="button w-100p m-b-1rem"
+            disabled={contract=="0xD5Dfb159788856f9fd5F897509d5a68b7b571Ea8"}
+        >
+            Quesadelon Musk
+        </button>
+        </Form>
+        <div style={{maxHeight: "300px", maxWidth: "100%", overflowY: 'scroll'}}>
+        <CodeSnippet fileName={contract} string={JSON.stringify(metaData, null, 2)}/>
+        </div>
+    </div>
+    <div style={{display: 'flex', maxWidth: '500px', justifyContent: 'center',height: '504px', margin: '2rem'}}>
+    <AnimatePresence exitBeforeEnter>
+        {<motion.img
+            key={metaData?.image}
+            alt={`${metaData?.image} nft image`}
+            initial="initial"
+            animate="in"
+            exit="out"
+            variants={{
+            initial: {
+                opacity: 0,
+                scale: '1%'
+            },
+            in: {
+                opacity: 1,
+                scale: '100%',
+            },
+            out: {
+                opacity: 0,
+                scale: '1%'
+            },
+            }}
+            transition={{
+            type: "spring",
+            duration: 1,
+            }}
+            className="ignore-markdown" 
+            style={{width: "20rem"}} 
+            src={metaData?.image}
+        />}
+    </AnimatePresence>
+    </div>
+    </div>
+  );
+}
+
