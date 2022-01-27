714dca1f919bd7268fba4c5103628fe76dbd67bd
jonathantcrawford <jon_t_crawford@protonmail.com>
2022-01-27 10:48:23 -0600

    debug

--- app/routes/blog/experiments-with-remix-and-cloudflare-workers.tsx
+++ app/routes/blog/experiments-with-remix-and-cloudflare-workers.tsx
@@ -2,7 +2,7 @@
    
 import { useEffect } from "react";
 import type { ActionFunction, LoaderFunction, MetaFunction } from "remix";
-import { json, Form, useLoaderData, Outlet } from "remix";
+import { json, Form, useLoaderData, Outlet, useCatch } from "remix";
 
 
 import { unencryptedSession } from "~/sessions.server";
@@ -130,15 +130,48 @@ export let loader: LoaderFunction = async ({ request }) => {
 };
 
 export default function Index() {
-  const body = useLoaderData();
+  //const body = useLoaderData();
 
-  const {metaData, contract} = body;
+  //const {metaData, contract} = body;
 
 
  
 
   return (
-    <Outlet context={{contract, metaData}}/>
+    <code>test</code>
   );
 }
 
+export function CatchBoundary() {
+  let caught = useCatch();
+
+  switch (caught.status) {
+    case 401:
+    case 404:
+      return (
+          <h1>
+            {caught.status} {caught.statusText}
+          </h1>
+      );
+
+    default:
+      throw new Error(
+        `Unexpected caught response with status: ${caught.status}`
+      );
+  }
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error);
+
+  return (
+    <div className="color-error font-sans-3 grid-area-content w-100p">
+      <h1 className="">App Error</h1>
+      <pre className="white-space-normal">{error.message}</pre>
+      <p>
+        Replace this UI with what you want users to see when your app throws
+        uncaught errors.
+      </p>
+    </div>
+  );
+}
